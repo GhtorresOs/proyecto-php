92697ebb259d1c4b35dcd1402159babd79b1eb15
GhtorresOs <gtgerardotorres@gmail.com>
2025-10-24 14:15:06 -0300

    creé ventas para visualizar los datos de cantidad total de articulos vendidos, falta implementar las consultas

---
+++ ventas.php
@@ -0,0 +1,101 @@
+<?php
+session_start();
+// Conexión a la base de datos 'prueba'
+$host = "localhost";
+$user = "root";
+$password = "";
+$dbname = "prueba";
+$conexion = new mysqli($host, $user, $password, $dbname);
+if ($conexion->connect_error) {
+  die("Error de conexión a la base de datos: " . $conexion->connect_error);
+}
+
+//CIERRE DE SESION
+$cerrarSesion = false;
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['cerrar_sesion'])) {
+  session_unset();
+  session_destroy();
+  $cerrarSesion = true;
+  header('Location: landing.html');
+  exit();
+}
+?>
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="UTF-8">
+        <title>Proyecto PHP</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    </head>
+    <body>
+    <!-- Navbar de la pagina -->
+        <nav class="navbar navbar-expand-lg bg-body-tertiary bg-dark border-bottom border-body" data-bs-theme="dark">
+            <div class="container-fluid">
+                <span class="navbar-brand mb-0 h1">Ventas</span>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                    <a class="btn btn-secondary me-3" href="index.php" role="button">Inicio</a>
+                    </li>
+                    <li class="nav-item">
+                        <?php if (isset($_SESSION['usuario_logeado'])): ?>
+                            <button type="button" class="btn btn-warning me-3" disabled><?php echo htmlspecialchars($_SESSION['usuario_logeado']); ?></button>
+                        <?php endif; ?> 
+                    </li>
+                </ul>
+                <form class="d-flex" role="search">
+                    <input class="form-control me-2" type="search" placeholder="Buscar" aria-label="Search">
+                    <button class="btn btn-success" type="submit">Buscar</button>
+                </form>
+                </div>
+            </div>
+        </nav>
+    <!-- Navbar de la pagina -->
+    <!-- Tarjetas de la pagina -->
+
+    <div class="container my-5" style="max-width: 1100px;">
+        <div class="card-group">
+
+            <div class="card text-white bg-secondary mx-2">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <h6 class="card-subtitle mb-2 text-body-secondary">Card subtitle</h6>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="card-link">Card link</a>
+                    <a href="#" class="card-link">Another link</a>
+                </div>
+            </div>
+
+            <div class="card mx-2 text-white bg-secondary">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <h6 class="card-subtitle mb-2 text-body-secondary">Card subtitle</h6>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="card-link">Card link</a>
+                    <a href="#" class="card-link">Another link</a>
+                </div>
+            </div>
+
+            <div class="card mx-2 text-white bg-secondary">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <h6 class="card-subtitle mb-2 text-white">Card subtitle</h6>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="card-link">Card link</a>
+                    <a href="#" class="card-link">Another link</a>
+                </div>
+            </div>
+
+        </div>
+    </div>
+    
+    <!-- Tarjetas de la pagina -->
+
+
+    </body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+</html>
